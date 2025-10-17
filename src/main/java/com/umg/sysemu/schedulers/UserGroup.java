444554c750ba217f3182ea5a46e9ed27000d1f2d
Daniel <da.aldanag@gmail.com>
2025-10-16 18:46:29 -0600

    Daniel: integracion de grupos de procesos

---
+++ src/main/java/com/umg/sysemu/schedulers/UserGroup.java
@@ -0,0 +1,21 @@
+package com.umg.sysemu.schedulers;
+
+import com.umg.sysemu.process.PCB;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserGroup {
+    private List<PCB> group;
+
+    public UserGroup(int algorithmQuantum) {
+        this.group = new ArrayList<PCB>();
+    }
+
+    public boolean isUserActive() { return !group.isEmpty(); }
+
+    public double getProcessQuantum(double groupShares) {
+
+    }
+
+}
